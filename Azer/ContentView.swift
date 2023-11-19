0ea0b5af8ce03c12c3775f7f219ea3efc2b00a68
gehad1995 <gehad.gehad1995@gmail.com>
2023-11-19 11:03:53 +0300

    test

--- Azer/ContentView.swift
+++ Azer/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("yo baby!!!")
+            Text("Jumanah")
         }
         .padding()
     }
