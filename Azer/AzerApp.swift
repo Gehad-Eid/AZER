320706a29539b2b2c44e117f78d41288cd5be20c
Gehad Eid <98451075+Gehad-Eid@users.noreply.github.com>
2023-11-19 10:15:09 +0300

    Add files via upload

---
+++ Azer/AzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzerApp.swift
+//  Azer
+//
+//  Created by Gehad Eid on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
