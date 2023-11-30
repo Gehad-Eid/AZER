8905f4af52682cc704d55d55fb4be5015ad8d74e
asmamohds <asmamohds.v@gmail.com>
2023-11-30 11:58:07 +0300

    challange pages + passed page

--- Azer/Views/challange0.swift
+++ Azer/Views/challange0.swift
@@ -1,18 +1,18 @@
 //
-//  test.swift
+//  challange0.swift
 //  Azer
 //
-//  Created by asma on 14/05/1445 AH.
+//  Created by asma on 16/05/1445 AH.
 //
 
 import SwiftUI
 
-struct test: View {
+struct challange0: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    test()
+    challange0()
 }
