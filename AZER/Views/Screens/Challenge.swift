46ed4f6fdcb513bbd0693365956521e3feeea818
gehad1995 <gehad.gehad1995@gmail.com>
2023-12-01 11:57:45 +0300

    challenge page

---
+++ AZER/Views/Screens/Challenge.swift
@@ -0,0 +1,18 @@
+//
+//  Challenge.swift
+//  AZER
+//
+//  Created by Gehad Eid on 01/12/2023.
+//
+
+import SwiftUI
+
+struct Challenge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Challenge()
+}
